abe104ce925899fbe73055bef96ea522fe9716ec
kien <kienyiep7@gmail.com>
2021-12-15 13:31:19 +0800

    Added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
+"""view set use the router to generate the different route which are available for our view set,
+with the view set you may be accessing the list request which is just the route of our API
+In this case, you would use a different URL then if you are accessing a specific object to do an update, delete, or a get  """
+
+"""Now, we will create a default router and register our ViewSet with the default router and pass the URLsw  into URL pattern"""
+router = DefaultRouter()
+"""Becsue the router will create all the four URLs for us, we dont need to specify a forward slash, when we define our view set URL name"""
+"""The base name will be used to retrieve the URL in our router if we ever need to do that using the URL retrieving function provided by django """
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 urlpatterns=[
-path('hello-view/', views.HelloApiView.as_view())
+path('hello-view/', views.HelloApiView.as_view()),
+path('',include(router.urls))
 ]
+""" So as we register the new route with our router, it generates a list of URLs that are associated for our view set it figures out the URLs """
+"""that are required for all of the functions that we add to our view set and then it generates this URL list"""
+"""which we can pass in using the path function and the include function to our URL patterns"""
+"""The reason that the blank string is specified here because we dont want to put a prefix to this URL, we just want to include all of the URLs in the base of this URLs file"""
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 """the status object from the rest framework is a list of handly http status codes that you can use when returning responses from your API."""
 from profiles_api import serializers
 """We will use this to tell the API what data to expect while making post,put and patch to our requests to our api"""
@@ -36,12 +37,56 @@ class HelloApiView(APIView):
 
     def put(self,request,pk=None):
          """Handle updating an object, by replacing the object with the object provided"""
-         return Response({'methos':'PUT'})
+         return Response({'method':'PUT'})
 
     def patch(self,request,pk=None):
           """Handle a partial update of an object, updating the field that is provided in the request"""
-          return Response({'methos':'PATCH'})
+          return Response({'method':'PATCH'})
 
     def delete(self,request,pk=None):
           """Delete an object"""
           return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        """Return a hello message"""
+        a_viewset =[
+        'Uses actions(list,create,retrieve, update,partial update)',
+        'Automatically maps to URLs using Routers',
+        'Provides more functionality with less code'
+
+        ]
+        return Response({'message':'Hello!','a_viewset':a_viewset})
+
+    def create(self,request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status= status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method' : 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
