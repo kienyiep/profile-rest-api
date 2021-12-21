df29e4d1af1cc4947c819ea44ddf9497567ff5d7
kien <kienyiep7@gmail.com>
2021-12-21 17:06:39 +0800

    Configured cors header

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -30,19 +30,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
-    'ec2-18-188-124-105.us-east-2.compute.amazonaws.com',
-    '127.0.0.1'
-]
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    'ec2-18-188-124-105.us-east-2.compute.amazonaws.com',
-    '127.0.0.1'
-    'http://localhost:3030',
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,10 +57,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'profiles_project.urls'
-CORS_ALLOWED_ORIGINS=True
-CORS_ALLOWED_ORIGIN_REGEXES=True
-CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS=True
+
 
 TEMPLATES = [
     {
