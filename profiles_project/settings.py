ad078de9d258941bbb287c97dfa6f2dc8b0506ec
kien <kienyiep7@gmail.com>
2021-12-22 23:36:25 +0800

    Configured for deployment.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'irov%c3wn78uylxsb#l&la%-h&$0f@!v*h_kduaru8hg8$eyc)'
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = [
-    'ec2-18-188-124-105.us-east-2.compute.amazonaws.com',
+    'ec2-3-17-23-32.us-east-2.compute.amazonaws.com'
     '127.0.0.1'
 ]
 
