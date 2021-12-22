f108b470e3aa51abd01783a71ce3bd45256ff8b2
kien <kienyiep7@gmail.com>
2021-12-22 23:28:03 +0800

    Configure for deployment.

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -7,7 +7,7 @@ Enable the django admin for our user profile model
 By default, the django admin is already enabled on all new projects however you need to register any newly created model with the django admin,
 so it knows that you want to display the model in the admin interface.
 """
-"""This tell the django admin to register our user profile model with the admin site so it makes it accessible through the admin interface """
+"""This will tell the django admin to register our user profile model with the admin site so it makes it accessible through the admin interface """
 admin.site.register(models.UserProfile)
 
 admin.site.register(models.userPhonebook)
